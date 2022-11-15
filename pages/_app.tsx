fa872c4bb6bc8d07fbe7a8e757694218c896e214
khunnunthawat <khunnunthawat@gmail.com>
2022-11-15 23:26:48 +0700

    notes-router-by-id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,9 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className='flex justify-center min-h-screen gap-6 mx-auto debug-screens bg-slate-100'>
-      <Component {...pageProps} />
+      <div className='sm:w-80 md:w-6/12'>
+        <Component {...pageProps} />
+      </div>
     </div>
   )
 }
---
+++ pages/note/[noteId].tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { ArrowUturnLeftIcon } from '@heroicons/react/24/solid'
+
+const Note = () => {
+  const router = useRouter()
+  const id = router.query.noteId
+  return (
+    <div>
+      <div className='flex w-full flex-row items-center justify-center relative my-8'>
+        <span className='inline-flex text-slate-700 text-lg font-semibold'>
+          Tasks
+        </span>
+
+        <Link href='/'>
+          <div className='left-0 top-0 absolute'>
+            <span className='self-center relative'>
+              <ArrowUturnLeftIcon className='h-6 w-6 text-slate-700  cursor-pointer' />
+            </span>
+          </div>
+        </Link>
+      </div>
+
+      <div className='flex w-full flex-col justify-between rounded-md bg-white py-5 px-4 mb-8'>
+        <div>
+          <h4 className='mb-3 font-bold text-gray-700'>title : {id}</h4>
+          <p className='text-sm text-gray-500'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+            saepe, iste voluptatum reprehenderit illum sed aliquid ad rerum?
+            Architecto est nesciunt tenetur praesentium minus sit facilis
+            explicabo ullam, aliquid ducimus.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Note
