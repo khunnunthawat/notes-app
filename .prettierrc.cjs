897259a9319f261e1d1a3a15c033c0328d70d348
khunnunthawat <khunnunthawat@gmail.com>
2022-11-15 15:52:12 +0700

    create-store-file

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  bracketSpacing: true,
+  tabWidth: 2,
+  jsxSingleQuote: true,
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import postReducer from '../store/reducers'
+const store = createStore(postReducer)
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
+
+// import '../styles/globals.css'
+// import type { AppProps } from 'next/app'
+// import { wrapper } from "../store/store";
+
+// function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+// export default wrapper.withRedux(App);
