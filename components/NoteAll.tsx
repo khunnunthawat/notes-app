7492d9bc9deb36e0a81ad2a283a30f2c02101af4
khunnunthawat <khunnunthawat@gmail.com>
2022-11-16 22:04:12 +0700

    debug-layout-grid

--- components/NoteAll.tsx
+++ components/NoteAll.tsx
@@ -1,27 +1,29 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import Link from 'next/link'
+import { connect } from 'react-redux'
 import { NotesProps } from '../types'
 
 const NoteAll = (props: any) => {
   return (
-    <div className='grid w-full gap-4 my-8 sm:grid-cols-2 xl:grid-cols-4'>
+    <div className='grid gap-4 my-8 sm:grid-cols-2 xl:grid-cols-4'>
       {props.notes.map((item: NotesProps, index: number) => (
         <Link href='/note/[noteId]' as={`note/${item.id}`} key={index}>
           <div
-            className='flex flex-col justify-between w-full px-4 py-5 mb-4 transition bg-white rounded-md cursor-pointer hover:shadow-md'
+            className='flex flex-col justify-between w-full col-auto row-auto mb-4 transition bg-white rounded-md cursor-pointer hover:shadow-md'
             onClick={() => props.dispatch({ type: 'EDIT_NOTE', id: item.id })}
           >
-            {item.title ? (
-              <h4 className='mb-3 font-bold text-gray-700 break-words'>
-                {item.title}
-              </h4>
-            ) : null}
-            {item.description ? (
-              <p className='text-sm text-gray-500 break-words'>
-                {item.description}
-              </p>
-            ) : null}
+            <div className='p-4'>
+              {item.title ? (
+                <h4 className='mb-3 font-bold text-gray-700 break-words'>
+                  {item.title}
+                </h4>
+              ) : null}
+              {item.description ? (
+                <p className='text-sm text-gray-500 break-words'>
+                  {item.description}
+                </p>
+              ) : null}
+            </div>
           </div>
         </Link>
       ))}
